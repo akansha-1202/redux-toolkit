84fb6954436bd303f79cfc85c7ae0029bfce0aaa
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-11-30 17:29:03 +0000

    update2

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,9 @@
 import React from "react"
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
+import {removeTodo} from "../features/todo/TodoSlice"
 export const Todos = () =>{
-    const todos = useSelector(state => state.todoReducer.todos)
+    const todos = useSelector(state => state.todoReducer.todos);
+    const dispatch = useDispatch();
     console.log(todos);
     return(
         <>
@@ -11,7 +13,10 @@ export const Todos = () =>{
                     <li key={item.id} className="w-2/3 flex justify-between px-4 py-2  bg-gray-400 my-4">
                         <div>{item.text}</div>
                         {/* <div>{item.completed}</div> */}
-                        <button className="border rounded px-2 py-0.5 bg-gray-400 hover:bg-gray-500">Delete</button>                
+                        <button 
+                        className="border rounded px-2 py-0.5 bg-gray-400 hover:bg-gray-500" 
+                        onClick={()=> dispatch(removeTodo(item.id))}
+                        >Delete</button>                
                     </li>
                 ))
             }

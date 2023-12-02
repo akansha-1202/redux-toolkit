7b3a6fd2ef8a4fbfb67953847e8f09cf4e0864cd
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-12-02 07:14:04 +0000

    added new

--- src/components/UpdateTodo.js
+++ src/components/UpdateTodo.js
@@ -22,7 +22,7 @@ const UpdateTodo = () => {
         navigate('/');
     }
   return (
-    <div className='bg-gray-400 h-screen flex items-center'>
+    <div className='bg-gray-200 h-screen flex items-center'>
       <form onSubmit={updateToHandler} className="w-full flex justify-center gap-x-5 py-6">
               <input type ="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder = "Enter a Todo...." className = "bg-gray-800 w-2/4 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 pl-2 leading-8 transition-colors duration-200 ease-in-out"/>
                   <button type="submit" className="text-white bg-indigo-400 hover:bg-indigo-500 border-0 rounded py-1 px-5 focus:outline-none">
